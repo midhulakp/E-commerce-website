709867a1e767b684738570f23412689e92890dbb
Midhula K P <kpmidhula7@gmail.com>
2025-01-09 17:57:47 +0530

    all files added

---
+++ script.js
@@ -0,0 +1,14 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const closebar = document.getElementById("close");
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (closebar) {
+  closebar.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
